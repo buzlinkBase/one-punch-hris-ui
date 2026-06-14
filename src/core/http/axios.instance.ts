d55f0b8c8f18cf72f5ac97ef4d9e51baf195fb53
jordantorreon <torreonjordan143@gmail.com>
2026-06-14 16:06:59 +0800

    remove url versioning in axios instance

--- src/core/http/axios.instance.ts
+++ src/core/http/axios.instance.ts
@@ -1,14 +1,15 @@
-import axios from 'axios';
-import { applyAuthInterceptor } from './interceptors/auth.interceptor';
-import { applyErrorInterceptor } from './interceptors/error.interceptor';
+import axios from "axios";
+import { applyAuthInterceptor } from "./interceptors/auth.interceptor";
+import { applyErrorInterceptor } from "./interceptors/error.interceptor";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL ?? 'http://localhost:1442/'}${import.meta.env.VITE_API_VERSION ?? 'v1'}/`,
+  // baseURL: `${import.meta.env.VITE_API_URL ?? "http://localhost:1442/"}${import.meta.env.VITE_API_VERSION ?? "v1"}/`,
+  baseURL: `${import.meta.env.VITE_API_URL ?? "http://localhost:1442/"}/`,
   headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-    'X-App-Version': '1.0.0',
-    'X-Client': 'hris-react',
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "X-App-Version": "1.0.0",
+    "X-Client": "hris-react",
   },
 });
 
