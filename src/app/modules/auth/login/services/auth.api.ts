5501f04c557e04197dddb8126caf04967c7e1493
jordantorreon <torreonjordan143@gmail.com>
2026-06-16 02:27:32 +0800

    refactor: set api base path

--- src/app/modules/auth/login/services/auth.api.ts
+++ src/app/modules/auth/login/services/auth.api.ts
@@ -2,10 +2,10 @@ import httpClient from "@/core/http/http-client";
 import type { LoginRequest } from "../models/api/request/login-request.model";
 import type { LoginResponse } from "../models/api/response/login-response.model";
 
-const ENDPOINT = `${import.meta.env.VITE_PREFIX_AUTH}/api/${import.meta.env.VITE_API_VERSION}/users/login`;
+const BASE_URL = `${import.meta.env.VITE_PREFIX_AUTH}/api/${import.meta.env.VITE_API_VERSION}/users`;
 
 export const authApi = {
   login(data: LoginRequest): Promise<LoginResponse> {
-    return httpClient.postUnwrapped<LoginResponse>(ENDPOINT, data);
+    return httpClient.postUnwrapped<LoginResponse>(`${BASE_URL}/login`, data);
   },
 };
