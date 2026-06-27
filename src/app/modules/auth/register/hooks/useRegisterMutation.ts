65b9c7b554a158383729d6a2676f436aa7089146
jhoncee <junnelhiro@hiro-c.net>
2026-06-27 10:38:02 +0800

    format

--- src/app/modules/auth/register/hooks/useRegisterMutation.ts
+++ src/app/modules/auth/register/hooks/useRegisterMutation.ts
@@ -1,6 +1,6 @@
-import { useMutation } from '@tanstack/react-query';
-import { authApi } from '@/app/modules/auth/login/services/auth.api';
-import type { RegisterRequest } from '../models/api/request/register-request.model';
+import { useMutation } from "@tanstack/react-query";
+import { authApi } from "@/app/modules/auth/login/services/auth.api";
+import type { RegisterRequest } from "../models/api/request/register-request.model";
 
 export function useRegisterMutation() {
   return useMutation({
