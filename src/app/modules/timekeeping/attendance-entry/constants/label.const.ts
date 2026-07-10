f2b990cc0ccce5f6bc2f06e0db4c09bbfe1e1a0f
jhoncee <junnel@hiro-c.net>
2026-07-10 12:39:22 +0800

    call api everytime clicking search

--- src/app/modules/timekeeping/attendance-entry/constants/label.const.ts
+++ src/app/modules/timekeeping/attendance-entry/constants/label.const.ts
@@ -1,6 +1,6 @@
 export const ATTENDANCE_ENTRY_LABEL = {
-  TITLE: "Attendance Entry",
-  CREATE_TITLE: "New Attendance Entry",
+  TITLE: "Manual Attendance Entry",
+  CREATE_TITLE: "New ManualAttendance Entry",
   FILTER_DATE_FROM: "Date From",
   FILTER_DATE_TO: "Date To",
   FILTER_EMPLOYEE: "Employee",
